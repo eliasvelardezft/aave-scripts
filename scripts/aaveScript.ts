ac9ce3799fe78878c68b5fc316007f14df0a1567
Elias Velardez <eliasvelardez38@gmail.com>
2022-11-15 22:41:45 -0300

    feat: add lendingPoolAddressesProvider to get lendingPool contract

--- scripts/aaveScript.ts
+++ scripts/aaveScript.ts
@@ -1,9 +1,32 @@
 import { ethers } from "hardhat";
+import { ILendingPoolAddressesProvider } from "../typechain-types/ILendingPoolAddressesProvider";
 import { getWETH } from "./getWETH";
+import { MAINNET_LENDINGPOOLADDRESSESPROVIDER_ADDRESS } from "../helper-hardhat-config";
+import { ILendingPool } from "../typechain-types";
 
 const main = async () => {
+	const { deployer } = await ethers.getNamedSigners();
+
 	const value = ethers.utils.parseEther("0.02");
 	await getWETH(value);
+
+	const lendingPool = await getLendingPool(deployer.address);
+	console.log("Lending pool address: ", lendingPool.address);
+};
+
+const getLendingPool = async (account: string): Promise<ILendingPool> => {
+	const lendingPoolAddressesProvider: ILendingPoolAddressesProvider = await ethers.getContractAt(
+		"ILendingPoolAddressesProvider",
+		MAINNET_LENDINGPOOLADDRESSESPROVIDER_ADDRESS!,
+		account
+	);
+	const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool();
+	const lendingPool: ILendingPool = await ethers.getContractAt(
+		"ILendingPool",
+		lendingPoolAddress,
+		account
+	);
+	return lendingPool;
 };
 
 main()
