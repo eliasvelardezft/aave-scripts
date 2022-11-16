9cc08ee37403c1664d3fbe3252f9dfb7cb2977a9
Elias Velardez <eliasvelardez38@gmail.com>
2022-11-16 01:11:24 -0300

    feat: implement aave lendingPool approval and deposit

--- scripts/aaveScript.ts
+++ scripts/aaveScript.ts
@@ -1,8 +1,12 @@
 import { ethers } from "hardhat";
 import { ILendingPoolAddressesProvider } from "../typechain-types/ILendingPoolAddressesProvider";
 import { getWETH } from "./getWETH";
-import { MAINNET_LENDINGPOOLADDRESSESPROVIDER_ADDRESS } from "../helper-hardhat-config";
-import { ILendingPool } from "../typechain-types";
+import {
+	MAINNET_LENDINGPOOLADDRESSESPROVIDER_ADDRESS,
+	WETH_MAINNET_ADDRESS,
+} from "../helper-hardhat-config";
+import { IERC20, ILendingPool } from "../typechain-types";
+import { BigNumber } from "ethers";
 
 const main = async () => {
 	const { deployer } = await ethers.getNamedSigners();
@@ -12,6 +16,24 @@ const main = async () => {
 
 	const lendingPool = await getLendingPool(deployer.address);
 	console.log("Lending pool address: ", lendingPool.address);
+
+	// approve the spender to use our weth
+	await approveErc20(WETH_MAINNET_ADDRESS!, lendingPool.address, value, deployer.address);
+	console.log("Approved!");
+
+	// deposit
+	await lendingPool.deposit(WETH_MAINNET_ADDRESS!, value, deployer.address, 0);
+	console.log("Deposited!");
+
+	// borrow
+	const { totalCollateralETH, availableBorrowsETH } = await lendingPool.getUserAccountData(
+		deployer.address
+	);
+	// how much we have as collateral?
+	console.log("total collateral: ", totalCollateralETH.toString());
+	// how much we can borrow?
+	console.log("available to borrow: ", availableBorrowsETH.toString());
+	// how much we have borrowed?
 };
 
 const getLendingPool = async (account: string): Promise<ILendingPool> => {
@@ -27,6 +49,22 @@ const getLendingPool = async (account: string): Promise<ILendingPool> => {
 		account
 	);
 	return lendingPool;
+};
+/**
+ * @param erc20Address: address of the erc20 token
+ * @param spenderAddress: contract we give our approval to spend our tokens
+ * @param amountToSpend: how much we approve the spender contract to spend
+ * @param account: our account
+ */
+const approveErc20 = async (
+	erc20Address: string,
+	spenderAddress: string,
+	amountToSpend: BigNumber,
+	account: string
+) => {
+	const erc20Token: IERC20 = await ethers.getContractAt("IERC20", erc20Address, account);
+	const tx = await erc20Token.approve(spenderAddress, amountToSpend);
+	await tx.wait(1);
 };
 
 main()
