abb8894335baa5116e3ab88cb11a818f61598416
Elias Velardez <eliasvelardez38@gmail.com>
2022-11-30 21:23:49 -0300

    feat: borrow and repay functionality

--- scripts/aaveScript.ts
+++ scripts/aaveScript.ts
@@ -4,8 +4,11 @@ import { getWETH } from "./getWETH";
 import {
 	MAINNET_LENDINGPOOLADDRESSESPROVIDER_ADDRESS,
 	WETH_MAINNET_ADDRESS,
+	MAINNET_AGGREGATORV3_ADDRESS,
+	DAI_TOKEN_MAINNET,
+	InterestRateMode,
 } from "../helper-hardhat-config";
-import { IERC20, ILendingPool } from "../typechain-types";
+import { IERC20, ILendingPool, AggregatorV3Interface } from "../typechain-types";
 import { BigNumber } from "ethers";
 
 const main = async () => {
@@ -25,15 +28,90 @@ const main = async () => {
 	await lendingPool.deposit(WETH_MAINNET_ADDRESS!, value, deployer.address, 0);
 	console.log("Deposited!");
 
+	const daiPrice = (await getDaiPriceInEth()).toNumber();
+	console.log("DAI price in ETH: ", (1e18 / daiPrice).toString());
+
+	// CHECK
+	await getUserData(lendingPool, deployer.address);
+
+	// get amountToBorrow in Wei, only .95 of the available amount
+	const { availableBorrowsETH } = await lendingPool.getUserAccountData(deployer.address);
+	// @ts-ignore
+	const amountToBorrow = availableBorrowsETH.toString() * 0.95 * (1 / daiPrice);
+	const amountToBorrowWei = ethers.utils.parseEther(amountToBorrow.toString());
+
 	// borrow
-	const { totalCollateralETH, availableBorrowsETH } = await lendingPool.getUserAccountData(
-		deployer.address
+	await borrowDai(DAI_TOKEN_MAINNET!, lendingPool, amountToBorrowWei, deployer.address);
+
+	// CHECK
+	await getUserData(lendingPool, deployer.address);
+
+	// repay
+	await repay(DAI_TOKEN_MAINNET!, lendingPool, amountToBorrowWei, deployer.address);
+
+	// CHECK
+	await getUserData(lendingPool, deployer.address);
+};
+
+const getUserData = async (lendingPool: ILendingPool, account: string) => {
+	const { totalCollateralETH, totalDebtETH, availableBorrowsETH } =
+		await lendingPool.getUserAccountData(account);
+	const daiPrice = 1 / (await getDaiPriceInEth()).toNumber();
+	// @ts-ignore
+	const collateralDai = totalCollateralETH * daiPrice;
+	// @ts-ignore
+	const debtDai = totalDebtETH * daiPrice;
+	// @ts-ignore
+	const availableBorrowDai = availableBorrowsETH * daiPrice;
+	console.log("total collateral in ETH: ", totalCollateralETH.toString());
+	console.log("total debt in ETH: ", totalDebtETH.toString());
+	console.log("available borrow in ETH: ", availableBorrowsETH.toString());
+	console.log("------------------------------");
+	console.log("total collateral in DAI: ", collateralDai.toString());
+	console.log("total debt in DAI: ", debtDai.toString());
+	console.log("available borrow in DAI: ", availableBorrowDai.toString());
+	console.log("*****************************");
+	console.log("*****************************");
+};
+
+const borrowDai = async (
+	daiAddress: string,
+	lendingPool: ILendingPool,
+	amountToBorrow: BigNumber,
+	account: string
+) => {
+	const interestRateMode = InterestRateMode.STABLE;
+	const borrowTx = await lendingPool.borrow(
+		daiAddress,
+		amountToBorrow,
+		interestRateMode,
+		0,
+		account
 	);
-	// how much we have as collateral?
-	console.log("total collateral: ", totalCollateralETH.toString());
-	// how much we can borrow?
-	console.log("available to borrow: ", availableBorrowsETH.toString());
-	// how much we have borrowed?
+	await borrowTx.wait(1);
+	console.log("Borrowed: ", amountToBorrow.toString());
+};
+
+const repay = async (
+	daiAddress: string,
+	lendingPool: ILendingPool,
+	amount: BigNumber,
+	account: string
+) => {
+	await approveErc20(daiAddress, lendingPool.address, amount, account);
+
+	const repayTx = await lendingPool.repay(daiAddress, amount, InterestRateMode.STABLE, account);
+	await repayTx.wait(1);
+	console.log("Repayed: ", amount.toString());
+};
+
+const getDaiPriceInEth = async () => {
+	const daiEthPriceFeed: AggregatorV3Interface = await ethers.getContractAt(
+		"AggregatorV3Interface",
+		MAINNET_AGGREGATORV3_ADDRESS!
+	);
+	const daiEthPrice = (await daiEthPriceFeed.latestRoundData())[1];
+	return daiEthPrice;
 };
 
 const getLendingPool = async (account: string): Promise<ILendingPool> => {
